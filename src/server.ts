3b24402213a1c1610078bc0a7626b1688f225cf9
subikstha <subsatch@gmail.com>
2024-05-27 20:58:35 +0545

    added and used morgan middleware

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
-import router from './router';
+import router from "./router";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.status(200);
   res.json({ message: "hello from server" });
 });
 
-app.use('/api', router)
+app.use("/api", router);
 
 export default app;
