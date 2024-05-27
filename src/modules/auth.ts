3f0b70124aabcdfc4d9df102fbdf184a2a782959
subikstha <subsatch@gmail.com>
2024-05-27 22:39:44 +0545

    added the protect middleware

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,9 +1,39 @@
 import jwt from "jsonwebtoken";
 
-export const createJWT = () => {
+export const createJWT = (user) => {
   const token = jwt.sign(
     { id: user.id, username: user.userName },
     process.env.JWT_SECRET
   );
   return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401);
+    res.json({ message: "not authorized" });
+    return;
+  }
+
+  //   We expect the bearer token to be like "Bearer <Token>" ,so we split it and destructure it
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401);
+    res.json({ message: "not valid token" });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401);
+    res.json({ message: "not valid token" });
+    return;
+  }
 };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./router";
 import morgan from "morgan";
+import { protect } from "./modules/auth";
 
 const app = express();
 
@@ -28,6 +29,6 @@ app.get("/", (req, res) => {
   res.json({ message: "hello" });
 });
 
-app.use("/api", router);
+app.use("/api", protect, router);
 
 export default app;
